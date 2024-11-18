3dc20526f201c69b8d2ec4d8ca2c144c1b343308
Kevin Pangestu <kevin.pangestu14@gmail.com>
2024-11-18 16:40:01 +0700

    fix: taskCard add dangerouslySetInnerHTML to ckeditor output

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 
-const TaskCard = ({ task }) => (
-  <div className="card bg-base-100 shadow-md">
-    <div className="card-body">
-      <h2 className="card-title">{task.title}</h2>
-      <p>{task.description}</p>
-      <div className="badge badge-outline">{task.tag}</div>
-      <div className="text-sm text-gray-500 mt-2">
-        {task.startDate} - {task.endDate}
+const TaskCard = ({ task }) => {
+  console.log("task description", task.description);
+
+  return (
+    <div className="card bg-base-100 shadow-md">
+      <div className="card-body">
+        <h2 className="card-title">{task.title}</h2>
+
+        <div dangerouslySetInnerHTML={{ __html: task.description }} />
+
+        <div className="badge badge-outline">{task.tag}</div>
+        <div className="text-sm text-gray-500 mt-2">
+          {task.startDate} - {task.endDate}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default TaskCard;
